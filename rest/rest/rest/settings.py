8286403191b08e731d5bb38bd29b43587d0709b9
Castle Crawford <nathanscottcrawford@gmail.com>
2016-09-16 09:51:41 -0500

    initial views setup complete

--- rest/rest/rest/settings.py
+++ rest/rest/rest/settings.py
@@ -42,12 +42,19 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+
+CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_WHITELIST = (
+    'localhost:8080',
+)
 
 ROOT_URLCONF = 'rest.urls'
 
---
+++ rest/rest/search/views.py
@@ -0,0 +1,69 @@
+from django.contrib.auth.models import User
+from django.contrib.auth import logout, login, authenticate
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
+from rest_framework import permissions
+from rest_framework import viewsets
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+from search.models import Parish, CompanyInfo, CompanyByParish
+from search.serializers import *
+
+
+class ParishList(viewsets.ModelViewSet):
+    model = Animal
+    queryset = Animal.objects.all()
+    serializer_class = AnimalSerializer
+
+
+
+class UserList(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+
+class CompanyInfoList(viewsets.ModelViewSet):
+    model = Ticket
+    queryset = Ticket.objects.all()
+    serializer_class = TicketSerializer
+
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+
+class CompanyByParish(viewsets.ModelViewSet):
+    queryset = Habitat.objects.all()
+    serializer_class = HabitatSerializer
+
+
+
+@csrf_exempt
+def login_user(request):
+    '''Handles the creation of a new user for authentication
+
+    Method arguments:
+      request -- The full HTTP request object
+    '''
+
+    # Load the JSON string of the request body into a dict
+    req_body = json.loads(request.body.decode())
+
+    # Use the built-in authenticate method to verify
+    authenticated_user = authenticate(
+            username=req_body['username'],
+            password=req_body['password']
+            )
+
+    # If authentication was successful, log the user in
+    success = True
+    if authenticated_user is not None:
+        login(request=request, user=authenticated_user)
+    else:
+        success = False
+
+    data = json.dumps({"success":success})
+    return HttpResponse(data, content_type='application/json')
+
+
