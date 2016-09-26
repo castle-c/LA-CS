6667225a09f9a30a15271ab622010f06abf74777
Castle Crawford <nathanscottcrawford@gmail.com>
2016-09-26 15:30:07 -0500

    fixed file structure

--- rest/rest/search/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Parish, CompanyInfo, CompaniesByParish
-
-admin.site.register(Parish)
-admin.site.register(CompanyInfo)
-admin.site.register(CompaniesByParish)
